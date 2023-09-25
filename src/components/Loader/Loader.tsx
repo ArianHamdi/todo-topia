e281ebcd9f640b18f5ba6cf9e111ad194e1224a4
Arian Hamdi <arian.hamdi@gmail.com>
2023-09-25 08:55:00 +0330

    fix: install dependency and remove some files

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,30 @@
+import { useSDK } from "@twa.js/sdk-react";
+import { PropsWithChildren } from "react";
+
+const Loader = ({ children }: PropsWithChildren<{}>) => {
+  const { didInit, components, error } = useSDK();
+
+  // There were no calls of SDK's init function. It means, we did not
+  // even try to do it.
+  if (!didInit) {
+    return <div>SDK init function is not yet called.</div>;
+  }
+
+  // Error occurred during SDK init.
+  if (error !== null) {
+    return <div>Something went wrong.</div>;
+  }
+
+  // If components is null, it means, SDK is not ready at the
+  // moment and currently initializing. Usually, it takes like
+  // several milliseconds or something like that, but we should
+  // have this check.
+  if (components === null) {
+    return <div>Warming up SDK.</div>;
+  }
+
+  // Safely render application.
+  return <>{children}</>;
+};
+
+export default Loader;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { SDKProvider } from "@twa.js/sdk-react";
+import Loader from "@/components/Loader";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SDKProvider>
+      <Loader>
+        <Component {...pageProps} />
+      </Loader>
+    </SDKProvider>
+  );
 }
