6a4a656638aa9477ba4d4115636a79ee601adffb
Arian Hamdi <arian.hamdi@gmail.com>
2023-09-25 19:50:49 +0330

    fix: initialize api route

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { validate } from "@twa.js/init-data-node";
 
 type Data = {
   name: string;
@@ -9,5 +10,10 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  try {
+    validate(req.headers.authorization!, process.env.BOT_API_TOKEN!);
+  } catch (err) {
+    console.log("err", err);
+  }
   res.status(200).json({ name: "John Doe" });
 }
