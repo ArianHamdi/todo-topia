c941215d87ef999cad30a70dd732e8db7b50877f
Arian Hamdi <arian.hamdi@gmail.com>
2023-09-26 02:01:12 +0330

    fix: svgr working now

--- next.config.js
+++ next.config.js
@@ -5,32 +5,19 @@ const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname)],
-    prependData: "@use 'styles' as *;",
+    prependData: "@use './src/styles' as *;",
   },
-  webpack(config) {
-    // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
-    );
-
-    config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
-      // Convert all other *.svg imports to React components
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: { not: /url/ }, // exclude if *.svg?url
-        use: ["@svgr/webpack"],
-      }
-    );
-
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i;
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+          },
+        },
+      ],
+    });
 
     return config;
   },
--- src/components/Dropdown/DropdownToggle/DropdownToggle.tsx
+++ src/components/Dropdown/DropdownToggle/DropdownToggle.tsx
@@ -1,5 +1,19 @@
+import { MouseEvent, Ref, forwardRef } from "react";
+import More from "@/assets/icons/more.svg";
 import styles from "./DropdownToggle.module.scss";
 
-const DropdownToggle = () => {};
+interface IProps {
+  onClick: () => void;
+}
 
-export default DropdownToggle;
+const DropdownToggle = ({ onClick }: IProps, ref: Ref<HTMLElement>) => {
+  // return <More className={styles.more} />;
+
+  return (
+    <button className={styles.toggle} onClick={onClick}>
+      <More className={styles.more} />
+    </button>
+  );
+};
+
+export default forwardRef(DropdownToggle);
