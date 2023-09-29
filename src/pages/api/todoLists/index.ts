b3be57ccf6a5d2ce6c137ea1bd862156b4309b05
Alireza Ghorbani <bounty.1379@gmail.com>
2023-09-29 13:10:39 +0330

    Updated data model to use userId as the reference field in User relation

--- src/pages/api/todoLists/index.ts
+++ src/pages/api/todoLists/index.ts
@@ -1,10 +1,24 @@
-// import withAuthorization from "@/utils/withAuthorization";
-// import { NextApiRequest, NextApiResponse } from "next";
+import withAuthorization from "@/utils/withAuthorization";
+import { NextApiRequest, NextApiResponse } from "next";
 
-// export default withAuthorization(function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse,
-//   user: any
-// ) {
-//   console.log(user);
-// });
+export default withAuthorization(function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  user: any
+) {
+  
+    switch (req.method) {
+        case 'GET':
+            
+            break;
+    
+        default:
+            break;
+    }
+
+
+
+
+
+
+});
