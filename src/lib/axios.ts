ecf9593b3eb80bc1876daecc2212b4fedc611265
Arian Hamdi <arian.hamdi@gmail.com>
2023-09-30 23:07:10 +0330

    fix: remove unnecessary code in axios config

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,17 +1,8 @@
 import axios from "axios";
 import { retrieveLaunchParams } from "@twa.js/sdk";
 
-const isServer = typeof window === "undefined";
-
-const initData = isServer
-  ? ""
-  : new URLSearchParams(window.location.hash.slice(1)).get("tgWebAppData");
-
 const instance = axios.create({
   baseURL: "/api",
-  headers: {
-    Authorization: `twa-init-data ${initData}`,
-  },
 });
 
 instance.interceptors.request.use((config) => {
