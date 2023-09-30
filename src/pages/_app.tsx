a3e017a53a74244564b787923f536f72119a9a93
Arian Hamdi <arian.hamdi@gmail.com>
2023-09-30 22:58:55 +0330

    fix: refactor twa sdk options and add react query provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,18 @@ import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { SDKProvider } from "@twa.js/sdk-react";
 import Loader from "@/components/Loader";
-import { InitOptions } from "@twa.js/sdk";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/react-query";
+import { sdkInitOptions } from "@/lib/twa-sdk";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const options: InitOptions = {
-    cssVars: true,
-  };
-
   return (
-    <SDKProvider initOptions={options}>
-      <Loader>
-        <Component {...pageProps} />
-      </Loader>
-    </SDKProvider>
+    <QueryClientProvider client={queryClient}>
+      <SDKProvider initOptions={sdkInitOptions}>
+        <Loader>
+          <Component {...pageProps} />
+        </Loader>
+      </SDKProvider>
+    </QueryClientProvider>
   );
 }
