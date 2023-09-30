1f60cdd292c8343bd55b0052f4b53b71e26d1203
Arian Hamdi <arian.hamdi@gmail.com>
2023-10-01 02:09:30 +0330

    feat: add react-query devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import Loader from "@/components/Loader";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/lib/react-query";
 import { sdkInitOptions } from "@/lib/twa-sdk";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </Loader>
       </SDKProvider>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
