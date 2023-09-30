0fd84dd91aa85a14c6dfe15ece11cc188bb8ff41
Arian Hamdi <arian.hamdi@gmail.com>
2023-09-30 22:52:18 +0330

    feat: add react-query config

---
+++ src/lib/react-query.ts
@@ -0,0 +1,10 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 1,
+      cacheTime: 1000 * 60 * 4,
+    },
+  },
+});
