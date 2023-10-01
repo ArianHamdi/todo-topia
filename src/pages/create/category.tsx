65b92eeed5d74d07b6e5322e60d79b6baf599b94
Arian Hamdi <arian.hamdi@gmail.com>
2023-10-01 13:40:28 +0330

    feat: add create pages routes

---
+++ src/pages/create/category.tsx
@@ -0,0 +1,5 @@
+const Category = () => {
+  return <h1>Category</h1>;
+};
+
+export default Category;
---
+++ src/pages/create/todo-list.tsx
@@ -0,0 +1,5 @@
+const TodoList = () => {
+  return <h1>TodoList</h1>;
+};
+
+export default TodoList;
