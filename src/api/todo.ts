1a1916e1c8d808df84e6493b54c6f807393b5d4f
Arian Hamdi <arian.hamdi@gmail.com>
2023-10-01 05:07:56 +0330

    fix: add some fetcher function and hook

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -1,6 +1,17 @@
 import axios from "@/lib/axios";
+import { ICategory, ICategoryPost } from "@/types";
 
 export const getCategories = async () => {
   const { data } = (await axios.get("/category")).data;
+  return data;
+};
+
+export const createCategory = async ({ title, color }: ICategoryPost) => {
+  const { data } = (
+    await axios.post("/category", {
+      title,
+      color,
+    })
+  ).data;
   return data;
 };
