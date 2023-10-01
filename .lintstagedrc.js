b21e6cb424db2ab99b1880dd634c19ae060e1b9c
Arian Hamdi <arian.hamdi@gmail.com>
2023-10-01 08:30:01 +0330

    fix: update lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  '**/*.(md)': filenames =>
+  '**/*.(md|json)': filenames =>
     `npx prettier --write --list-different ${filenames.join(' ')}`,
 
-  '**/*.(ts|tsx|js|jsx|json)': filenames => [
-    // `npx eslint ${filenames.join(" ")} --fix`,
+  '**/*.(ts|tsx|js|jsx)': filenames => [
+    `npx eslint ${filenames.join(' ')} --fix`,
     `npx prettier --write --list-different ${filenames.join(' ')}`,
   ],
 };
