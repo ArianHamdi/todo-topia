e5f2774ba5cc8b92b2e645508b69f0382cef4e1b
Arian Hamdi <arian.hamdi@gmail.com>
2023-10-02 16:57:01 +0330

    fix: type

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -2,7 +2,7 @@ import axios from '@/lib/axios';
 import { ICategory, ICategoryPost } from '@/types';
 
 export const getCategories = async () => {
-  const { data } = (await axios.get('/category')).data;
+  const { data } = await axios.get<ICategory[]>('/category');
   return data;
 };
 
