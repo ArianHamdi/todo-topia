4bd5167e98900a3bdd0bd7769a710f7ace26b9e0
Alireza Ghorbani <bounty.1379@gmail.com>
2023-10-03 12:50:39 +0330

    feat: added post types and joi schema

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,7 +1,19 @@
-import { ICategoryPost } from '@/types';
+import { ICategoryPost, ITaskPost, ITodoListPost } from '@/types';
 import Joi from 'joi';
 
 export const categorySchema = Joi.object<ICategoryPost>({
   color: Joi.string().required(),
   title: Joi.string().required(),
+});
+
+export const todoListSchema = Joi.object<ITodoListPost>({
+  title: Joi.string().required(),
+  categoryId: Joi.string().required(),
+});
+
+export const taskSchema = Joi.object<ITaskPost>({
+  title: Joi.string().required(),
+  description: Joi.string(),
+  deadline: Joi.date(),
+  repeat: Joi.string().valid('daily', 'weekly', 'monthly'),
 });
