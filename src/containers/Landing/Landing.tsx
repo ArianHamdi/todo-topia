0c1c5c197f00d3c835a7aa22da0dc45bd353e021
Arian Hamdi <arian.hamdi@gmail.com>
2023-10-04 06:43:52 +0330

    feat: add some utility

--- src/containers/Landing/Landing.tsx
+++ src/containers/Landing/Landing.tsx
@@ -5,10 +5,20 @@ import { useTranslation } from '@/hooks/useTranslation';
 import FloatingActionButton from '@/components/FloatingActionButton';
 import FloatingActionButtonItem from '@/components/FloatingActionButton/FloatingActionButtonItem';
 import Add from '@/assets/icons/add.svg';
+import { useMainButton } from '@/hooks/useMainButton';
 
 const Landing = () => {
   const { data } = useCategories();
   const { t } = useTranslation();
+
+  useMainButton({
+    text: 'salam2',
+    backgroundColor: '#a12122',
+
+    onClick: () => {
+      console.log('landing');
+    },
+  });
 
   const items = data?.map(category => (
     <Category key={category.id} {...category} />
---
+++ src/hooks/useMainButton.ts
@@ -0,0 +1,85 @@
+import { useEffect, useMemo } from 'react';
+import { useMainButton as useMainButtonReact } from '@twa.js/sdk-react';
+import { RGB } from '@twa.js/colors';
+
+interface IProps {
+  isVisible?: boolean;
+  isLoading?: boolean;
+  isEnabled?: boolean;
+  backgroundColor?: RGB;
+  disableBackgroundColor?: RGB;
+  textColor?: RGB;
+  text?: string;
+  onClick?: () => void;
+}
+
+export const useMainButton = ({
+  isVisible = true,
+  isLoading = false,
+  isEnabled = true,
+  backgroundColor,
+  disableBackgroundColor,
+  textColor,
+  text,
+  onClick = () => {},
+}: IProps) => {
+  const mainButton = useMainButtonReact();
+
+  // useEffect(() => {
+  //   if (backgroundColor) {
+  //     mainButton.setBackgroundColor(backgroundColor);
+  //   }
+  // }, [backgroundColor, mainButton]);
+
+  useEffect(() => {
+    if (isVisible) {
+      mainButton.show();
+    }
+    return () => {
+      mainButton.hide();
+    };
+  }, [isVisible, mainButton]);
+
+  useEffect(() => {
+    if (isLoading) {
+      mainButton.showProgress();
+    }
+    return () => {
+      mainButton.hideProgress();
+    };
+  }, [isLoading, mainButton]);
+
+  useEffect(() => {
+    if (isEnabled) {
+      mainButton.enable();
+      if (backgroundColor) {
+        mainButton.setBackgroundColor(backgroundColor);
+      }
+    }
+    return () => {
+      mainButton.disable();
+      if (disableBackgroundColor) {
+        mainButton.setBackgroundColor(disableBackgroundColor);
+      }
+    };
+  }, [isEnabled, backgroundColor, disableBackgroundColor, mainButton]);
+
+  useEffect(() => {
+    if (textColor) {
+      mainButton.setTextColor(textColor);
+    }
+  }, [textColor, mainButton]);
+
+  useEffect(() => {
+    if (text) {
+      mainButton.setText(text);
+    }
+  }, [text, mainButton]);
+
+  useEffect(() => {
+    mainButton.on('click', onClick);
+    return () => {
+      mainButton.off('click', onClick);
+    };
+  }, [mainButton]);
+};
