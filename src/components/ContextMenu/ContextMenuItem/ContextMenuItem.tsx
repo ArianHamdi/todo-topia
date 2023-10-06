11a52d586bb9da3289eead3655c122c15b78c308
Arian Hamdi <arian.hamdi@gmail.com>
2023-10-06 20:24:54 +0330

    feat: implement remove category

--- src/components/ContextMenu/ContextMenuItem/ContextMenuItem.tsx
+++ src/components/ContextMenu/ContextMenuItem/ContextMenuItem.tsx
@@ -1,14 +1,25 @@
-import { ReactNode } from 'react';
+import { ReactNode, MouseEvent } from 'react';
 import styles from './ContextMenuItem.module.scss';
 
 export interface IContextMenuItem {
   variant?: 'info' | 'danger';
   children: ReactNode;
-  icon?: ReactNode;
+  icon: ReactNode;
+  onClick: (event: MouseEvent<HTMLLIElement>) => void;
 }
 
-const ContextMenuItem = ({ variant = 'info', children }: IContextMenuItem) => {
-  return <li>{children}</li>;
+const ContextMenuItem = ({
+  variant = 'info',
+  icon,
+  children,
+  onClick,
+}: IContextMenuItem) => {
+  return (
+    <li className={styles.item} data-variant={variant} onClick={onClick}>
+      {icon}
+      <span className={styles.label}>{children}</span>
+    </li>
+  );
 };
 
 export default ContextMenuItem;
