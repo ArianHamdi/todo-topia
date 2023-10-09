68ae9b6336d07653f8d14b9f6f3bc8cf7936dcb4
Alireza Ghorbani <bounty.1379@gmail.com>
2023-10-09 12:09:05 +0330

    feat: added comments to API's

--- src/pages/api/category/index.ts
+++ src/pages/api/category/index.ts
@@ -1,19 +1,22 @@
 import prisma from '@/lib/prisma';
 import { categorySchema } from '@/schema';
-import { ICategory } from '@/types';
 import withAuthorization from '@/utils/withAuthorization';
-import { Category } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+// Define and export an async function as the API handler, wrapped in authorization middleware
 export default withAuthorization(async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
   userId: any
 ) {
   try {
-    console.log('category', userId);
+    console.log('category', userId); // Log the user ID for debugging
+
+    // Use a switch statement to handle different HTTP methods
     switch (req.method) {
       case 'GET':
+        // Handle HTTP GET request
+        // Fetch categories associated with the user and include associated todoLists
         const data = await prisma.category.findMany({
           where: {
             userId,
@@ -23,14 +26,17 @@ export default withAuthorization(async function handler(
           },
         });
 
-        res.status(200).json(data);
+        res.status(200).json(data); // Respond with the fetched data
         break;
 
       case 'POST':
-        const { color, title } = req.body;
+        // Handle HTTP POST request
+        const { color, title } = req.body; // Extract color and title from the request body
 
+        // Validate the extracted data using the categorySchema
         await categorySchema.validate({ color, title });
 
+        // Create a new category in the database
         const newCategory = await prisma.category.create({
           data: {
             color,
@@ -39,14 +45,14 @@ export default withAuthorization(async function handler(
           },
         });
 
-        res.status(201).json(newCategory);
+        res.status(201).json(newCategory); // Respond with the newly created category
         break;
 
       default:
         break;
     }
   } catch (error) {
-    console.log(error);
-    res.status(400).json('Error');
+    console.log(error); // Log any errors that occur during execution
+    res.status(400).json('Error'); // Respond with a 400 status code and an error message
   }
 });
