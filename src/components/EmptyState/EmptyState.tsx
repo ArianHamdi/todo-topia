1d12a4020ad5791f33c2a0982da9dccab17a8166
Alireza Ghorbani <bounty.1379@gmail.com>
2023-10-10 21:12:22 +0330

    feat: added empty state component to pages

--- src/components/EmptyState/EmptyState.tsx
+++ src/components/EmptyState/EmptyState.tsx
@@ -1,7 +1,14 @@
 import styles from './EmptyState.module.scss';
+import Icon from '@/assets/icons/bots.svg';
 
 const EmptyState = () => {
-  return <div>EmptyState</div>;
+  return (
+    <div className={styles.emptyState}>
+      <div className={styles.message}>
+        <Icon width={50} height={50} className={styles.icon} /> Empty!
+      </div>
+    </div>
+  );
 };
 
 export default EmptyState;
