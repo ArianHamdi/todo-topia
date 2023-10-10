d016edaa714dec4d9ee711810d1cb81a0c8db0f4
Alireza Ghorbani <bounty.1379@gmail.com>
2023-10-10 21:42:07 +0330

    add empty to translations

--- src/utils/withAuthorization.ts
+++ src/utils/withAuthorization.ts
@@ -3,24 +3,32 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '@/lib/prisma';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
+// This function is a middleware that adds authorization checks to your Next.js API route.
 export default function withAuthorization(next: Function) {
   return async function (req: NextApiRequest, res: NextApiResponse) {
     try {
+      // Validate the authorization header using the BOT_API_TOKEN.
       validate(req.headers.authorization!, process.env.BOT_API_TOKEN!);
+
+      // Parse the user data from the authorization header.
       const urlParams = new URLSearchParams(req.headers.authorization);
       const userData = JSON.parse(urlParams.get('user')!);
       console.log(userData.id);
+
       if (req.cookies.token) {
+        // If a token exists in cookies, verify it using the BOT_API_TOKEN.
         const decoded = jwt.verify(
           req.cookies.token,
           process.env.BOT_API_TOKEN!
         ) as JwtPayload;
       } else {
+        // If there's no token in cookies, create a new token and associate it with the user.
         const signToken = jwt.sign(
           { id: userData.id },
           process.env.BOT_API_TOKEN!
         );
 
+        // Check if the user exists in the database, and create if not.
         const user = await prisma.user.findUnique({
           where: {
             userId: userData.id.toString(),
@@ -46,13 +54,17 @@ export default function withAuthorization(next: Function) {
           });
         }
 
+        // Set the newly created token in cookies for future requests.
         res.setHeader('Set-Cookie', `token=${signToken}; Path=/; HttpOnly`);
       }
+
+      // Call the next middleware or API handler with the user's ID.
       next(req, res, userData.id.toString());
     } catch (error) {
       console.log(error);
       console.log('Unauthorized user');
 
+      // Respond with a 401 Unauthorized status and an error message.
       res.status(401).json({
         error: 'Unauthorized user',
       });
